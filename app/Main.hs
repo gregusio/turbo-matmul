4f9ad3828e9454fa113446ab4453ed06270fc854
gregusio <grzegorzdynaq@gmail.com>
2025-06-08 20:18:38 +0200

    Refactor matrix multiplication selection process in main function

--- app/Main.hs
+++ app/Main.hs
@@ -13,18 +13,31 @@ main = do
     let (Matrix n1 m1 _) = matrix1
     let (Matrix n2 m2 _) = matrix2
 
-    putStrLn "Starting matrix multiplication..."
-    start <- getCurrentTime
-    let simpleMatrixMultiplication = multiplyMatrices matrix1 matrix2
-    writeMatrixToFile "output/result1.txt" simpleMatrixMultiplication
-    end <- getCurrentTime
-    putStrLn $ "Simple multiplication time: " ++ show (diffUTCTime end start)
+    putStrLn $ "Choose multiplication method for matrices:"
+    putStrLn "1. Simple multiplication"
+    putStrLn "2. Fast multiplication"
+    choice <- getLine
 
-    putStrLn "Starting fast matrix multiplication..."
-    start <- getCurrentTime
-    let extendedMatrix1 = extendToTwoPowerSquare matrix1
-        extendedMatrix2 = extendToTwoPowerSquare matrix2
-        result = backToPreviousSize n1 m2 (fastMatrixMultiplication extendedMatrix1 extendedMatrix2)
-    writeMatrixToFile "output/result2.txt" result
-    end <- getCurrentTime
-    putStrLn $ "Fast multiplication time: " ++ show (diffUTCTime end start)
+    case choice of
+        "1" -> do
+            putStrLn "You chose simple multiplication."
+            putStrLn "Starting simple matrix multiplication..."
+            start <- getCurrentTime
+            let simpleMatrixMultiplication = multiplyMatrices matrix1 matrix2
+            writeMatrixToFile "output/result1.txt" simpleMatrixMultiplication
+            end <- getCurrentTime
+            putStrLn $ "Simple multiplication time: " ++ show (diffUTCTime end start)
+        "2" -> do
+            putStrLn "You chose fast multiplication."
+            putStrLn "Starting fast matrix multiplication..."
+            start <- getCurrentTime
+            let extendedMatrix1 = extendToTwoPowerSquare matrix1
+                extendedMatrix2 = extendToTwoPowerSquare matrix2
+                result = backToPreviousSize n1 m2 (fastMatrixMultiplication extendedMatrix1 extendedMatrix2)
+            writeMatrixToFile "output/result2.txt" result
+            end <- getCurrentTime
+            putStrLn $ "Fast multiplication time: " ++ show (diffUTCTime end start)
+        _   -> do 
+            putStrLn "Invalid choice. Please choose 1 or 2."
+            main
+    
